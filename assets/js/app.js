af5413752eff8f9a286fae1fcc2d6d02162306d8
Luqman Mohamed <lukas.woodman@gmail.com>
2016-08-20 14:31:13 +0800

    Testing templates without node

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
-//var theData = {headerT}
+$(document).ready(function() {
+  var $mainTemplate = $("#main-template").html();
+  //Compile main template
+  var template = Handlebars.compile($mainTemplate);
+  //Get template from server
+  $.get("../../template.html").done(function(response) {
+    var content = $($.parseHTML(response));
+
+    //You need to register each partial
+    Handlebars.registerPartial({
+      navbar: content.filter("#navbar-partial").html(),
+      footer: content.filter("#footer-partial").html(),
+      home: content.filter("#home-partial").html(),
+      signup: content.filter("#signup-partial").html()
+    });
+
+    //Your data
+    var data = {};
+
+
+    document.body.innerHTML = template(data);
+
+  });
+});
