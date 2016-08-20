4e260008428e60d14fc5b29e6161ff22bc0624c4
Luqman Mohamed <lukas.woodman@gmail.com>
2016-08-20 13:41:51 +0800

    Initialized node

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,10 @@
-//var theData = {headerT}
+$(document).ready(function() {
+
+  $.get("../../templates.html").done(function(response) {
+  var content = $($.parseHTML(response));
+
+  var body = Handlebars.compile($("#yield").html());
+    Handlebars.registerPartial("navbar", $("#navbar-partial").html());
+
+    template(yourData);
+});
