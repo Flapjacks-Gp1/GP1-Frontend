b0a841983155b57ca7e9ada34b760a94cf9be204
Gabrielle Chung <gabrielle.chung.g@gmail.com>
2016-08-20 16:20:06 +0800

    basic createevent html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,48 @@
-$(document).ready(function() {
-  var $mainTemplate = $("#main-template").html();
-  //Compile main template
-  var template = Handlebars.compile($mainTemplate);
-  //Get template from server
-  $.get("../../template.html").done(function(response) {
-    var content = $($.parseHTML(response));
 
-    //You need to register each partial
-    Handlebars.registerPartial({
-      navbar: content.filter("#navbar-partial").html(),
-      footer: content.filter("#footer-partial").html(),
-      home: content.filter("#home-partial").html(),
-      signup: content.filter("#signup-partial").html(),
-      login: content.filter("#login-partial").html()
-    });
-
-    //Your data
-    var data = {};
+(function($) {
 
 
-    document.body.innerHTML = template(data);
+  // 2. changing to jquery format, notice we don't define the XHR class anymore
 
-  });
-});
+  var $eventname = $('.eventname');
+  var $description = $('.description');
+  var $location = $('.location');
+  var $loader = $('.loader');
+
+  var server_url = 'https://crossorigin.me/https://limitless-brook-89760.herokuapp.com/gabrielle';
+
+  $.ajax({
+    url: server_url,
+    method: "post",
+    dataType: 'json',
+    // show the loader before making the request
+    beforeSend: function() {
+      $loader.show();
+    }
+  }).done(successFunction)
+    .fail(failFunction)
+    .always(alwaysFunction);
+
+
+    function successFunction(data) {
+    $.css('border', '1px solid #e8e8e8');
+    // if data exists
+    console.log(data);
+
+    $eventname.text(data.name);
+    $description.text(data.email);
+    $location.text(data.hobbies);
+  }
+
+  function failFunction(request, textStatus, errorThrown){
+    $name.text('An error occurred during your request: ' +  request.status + ' ' + textStatus + ' ' + errorThrown);
+  }
+
+  // always function
+  function alwaysFunction() {
+    // hide the loader
+    $loader.hide();
+    // $body.css('overflow', 'hidden');
+  }
+
+})(jQuery);
