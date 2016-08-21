758f74538d114276f366ae690056c1a9034f8299
kit jonathan ho <j.hokit80@gmail.com>
2016-08-21 12:47:55 +0800

    Added package.json

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var ejs = require("ejs");
+var bodyParser  = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//routing
+
+app.get("/", function(req, res){
+  res.send("this is the home page");
+});
+
+
+
+
+
+
+
+
+
+
+
+//Server
+app.set('port', (process.env.PORT || 7000));
+
+app.listen(app.get('port'), function() {
+  console.log('My express server is running at localhost', app.get('port'));
+});
+
+module.exports = app;
