a26d8ea212b3f5e47ff1e4989ef624c461ec23f4
Luqman Mohamed <lukas.woodman@gmail.com>
2016-08-22 19:28:26 +0800

    Fixed create events

--- public/js/createevent.js
+++ public/js/createevent.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
   var server_url = 'http://localhost:9000/api/events';
 
-localStorage.setItem('token', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3Yjk1NmEyNzA1OGYwNTExOGU1ODdlYyIsImVtYWlsIjoiam9uYXRoYW5AZ21haWwuY29tIiwiaWF0IjoxNDcxODQ2NzMyLCJleHAiOjE0NzE4NTc1MzJ9._x0UPjHICYKAhJCvHJCewPniF4Wvl2Z8VlYb5muFZus");
-
 var userId = function() {
   return localStorage.getItem('user_id');
 };
@@ -29,7 +27,7 @@ $('#createform').on('submit',function(e){
       // }
     }).done(function successFunction(data, res) {
        console.log(res);
-        window.location.replace("/events");
+        window.location.replace("/events/" + data._id);
       })
       .fail(failFunction)
       .always(alwaysFunction);
