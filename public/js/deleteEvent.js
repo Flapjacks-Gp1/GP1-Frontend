fa963825975d71126f3f4eaec005b2d5497eeca2
Sabrina Ooi <heysabs@gmail.com>
2016-08-23 14:41:14 +0800

    updated event edit and delete

--- public/js/deleteEvent.js
+++ public/js/deleteEvent.js
@@ -1,28 +1,27 @@
 $(document).ready(function () {
     var client_url = 'http://localhost:7000/'
     var event_url = 'http://localhost:9000/api/events/';
-    var $editEvent = $('#editEvent');
+    var $deleteEvent = $('#deleteEvent');
     var eventPathname = window.location.pathname.split('/');
     console.log(eventPathname[2]);
 
-  $editEvent.submit(function(e){
-
+  $deleteEvent.on("click", function(e){
+console.log(event_url + eventPathname[2]);
     e.preventDefault();
 
     $.ajax({
       url: event_url + eventPathname[2],
-      method: "POST",
+      method: "DELETE",
       headers: {
              "Authorization": "Bearer " + localStorage.getItem('token')
-           },
-      dataType: 'json',
-      data: $editEvent.serialize()
+           }
     }).done(successFunction)
       .fail(failFunction)
       .always(alwaysFunction);
 
 
     function successFunction(data) {
+      console.log("DELETED SUCCESSFULLY");
       location.href = client_url + 'events';
     }
 
@@ -34,5 +33,5 @@ $(document).ready(function () {
     // always function
     function alwaysFunction() {
     }
-  })
-})
+  });
+});
