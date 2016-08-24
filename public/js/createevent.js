c183ffd0006503e85a60447261ffecee39a48fde
Luqman Mohamed <lukas.woodman@gmail.com>
2016-08-24 22:14:47 +0800

    added image uploader to the event creation page

--- public/js/createevent.js
+++ public/js/createevent.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   var event_url = server_url + 'events/';
+  var $imageUploader = $("#imageUploader");
+  var $uploadSubmit = $("#uploadSubmit");
+  var uploadLink = 'https://api.imgur.com/3/image';
+  var clientId = 'aca6d2502f5bfd8';
+  var $imgPreview = $('#imgPreview');
+  var $avatarUrl = $('#avatarUrl');
 
 var userId = function() {
   return localStorage.getItem('user_id');
@@ -7,6 +13,39 @@ var userId = function() {
 console.log(userId());
 
 $("#eventCreator").attr("value", localStorage.getItem('user_id'));
+
+//Image upload trigger
+$uploadSubmit.on('click', function(e) {
+  e.preventDefault();
+  //  var imgData = $imageUploader[0].files[0];
+   var imgData = new FormData();
+   imgData.append("image", $imageUploader[0].files[0]);
+
+  console.log($imageUploader.val());
+  $.ajax({
+    url: uploadLink,
+    type: "POST",
+    headers: {
+      'Authorization': 'Client-ID '+ clientId
+    },
+    data: imgData,
+    contentType: false,
+    processData: false,
+  }).done(function(result) {
+      console.log(result.data);
+      var imgId = result.data.id;
+      var imgLink = result.data.link;
+      $imgPreview.css('display', 'block');
+      $imgPreview.html("<img src='http://i.imgur.com/"+imgId+".png'>");
+      $avatarUrl.val(imgLink)
+    })
+    .fail(function() {
+
+    })
+    .always(function() {
+
+    });
+});
 
 //Create New Event
 $('#createform').on('submit',function(e){
