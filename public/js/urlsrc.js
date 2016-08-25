45e64e16562e5884a92bd4465496e6d4a879e8bc
Luqman Mohamed <lukas.woodman@gmail.com>
2016-08-25 11:51:42 +0800

    switched to deployment

--- public/js/urlsrc.js
+++ public/js/urlsrc.js
@@ -1,3 +1,3 @@
 
-  // var server_url = 'https://jio-app.herokuapp.com/api/';
-  var server_url = 'http://localhost:9000/api/';
+  var server_url = 'https://jio-app.herokuapp.com/api/';
+  // var server_url = 'http://localhost:9000/api/';
